b9f9f5cdc49c81c79c4e8d0bdedb6fd0d92b2650
Monetthigpen <monet.thigpen@gmail.com>
2019-04-03 16:52:34 -0400

    Updates

---
+++ precache-manifest.142004154d0051f8d17bac186f2c5394.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef5b5b70572171fce077",
+    "url": "/clickygame2/static/js/runtime~main.c56e6363.js"
+  },
+  {
+    "revision": "5dbc73edce0f37196498",
+    "url": "/clickygame2/static/js/main.5fdd8847.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickygame2/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "5dbc73edce0f37196498",
+    "url": "/clickygame2/static/css/main.a9e31254.chunk.css"
+  },
+  {
+    "revision": "3901cab763afcef171508cf6ee843769",
+    "url": "/clickygame2/index.html"
+  }
+];
